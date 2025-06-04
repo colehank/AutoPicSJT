ab8ef4ff99d597cc7d2491cc041df8b0aa42653d
Guohao Zhang <72941250+colehank@users.noreply.github.com>
2025-06-04 19:28:03 +0800

    Revert "Rename image_utils"

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,4 +2,3 @@ from __future__ import annotations
 
 from .graph_utils import *
 from .llm_utils import *
-from .image_utils import make_sequence
--- test_image_gen.py
+++ test_image_gen.py
@@ -7,7 +7,39 @@ import os.path as op
 from PIL import Image
 from PIL import ImageOps
 
-from src.utils.image_utils import make_sequence
+def make_sequence(image_list, border_width=10, border_color='white'):
+    """
+    Concatenate images horizontally.
+
+    Parameters:
+    -----------
+    image_list: list
+        A list of images to concatenate.
+    border_width: int
+        The width of the border.
+    border_color: str
+        The color of the border.
+
+    Returns:
+    --------
+    new_im: Image
+        The concatenated image.
+    """
+    bordered_images = [
+        ImageOps.expand(im, border=border_width, fill=border_color) for im in image_list
+    ]
+
+    total_width = sum(im.width for im in bordered_images)
+    max_height = max(im.height for im in bordered_images)
+
+    new_im = Image.new('RGBA', (total_width, max_height), border_color)
+
+    x_offset = 0
+    for im in bordered_images:
+        new_im.paste(im, (x_offset, 0))
+        x_offset += im.width
+
+    return new_im
 
 def load_vng_pics(trait, itemID, res_dir):
     des_dir = f'{res_dir}/{trait}/{trait}_{itemID}'
